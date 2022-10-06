0e85990daddb441ba3d3307ab86b3fe116ddf8ac
Samuel Lando <samuel.lando@aol.com>
2022-10-05 23:43:41 -0400

    Lint and type

--- src/anyrest/firestore_odr.py
+++ src/anyrest/firestore_odr.py
@@ -1,24 +1,32 @@
 from google.cloud import firestore
+from google.cloud.firestore import DocumentReference
+from typing import Optional, Any, Union
 import os
 
 PROJECT = os.environ.get("PROJECT")
 
+dict = dict[str, Any]
+
+
 class Odr:
-    def __init__(self, db=None):
+    def __init__(self, db: Optional[firestore.Client] = None):
         if db is None:
             self.db = firestore.Client(project=PROJECT)
         else:
             self.db = db
 
-    def _dig(self, doc_ref, depth=0):
+    def _dig(self, doc_ref: DocumentReference, depth: int = 0) -> dict:
         doc = doc_ref.get()
         cols = doc_ref.collections()
-        if doc.exists:
-            data = doc.to_dict()
+        data: Union[dict, None] = doc.to_dict()
+
+        if data is None:
+            # If the document does not exist.
+            data = {}
         else:
             if depth == 0 and len(list(cols)) == 0:
                 raise KeyError
-            data = {}
+
         for col in cols:
             data[col.id] = {}
             for d in col.list_documents():
@@ -26,17 +34,19 @@ class Odr:
 
         return data
 
-    def _getDoc(self, doc_ref, recursive):
+    def _getDoc(self, doc_ref: DocumentReference, recursive: bool) -> dict:
         if recursive:
             return self._dig(doc_ref)
         else:
             data = doc_ref.get()
-            if not data.exists:
+            d: Union[dict, None] = data.to_dict()
+            if d is None:
+                # If document does not exist.
                 raise KeyError
             else:
-                return data.to_dict()
+                return d
 
-    def get(self, path, recursive=True):
+    def get(self, path: str, recursive: bool = True) -> dict:
         if len(path.split("/")) % 2 == 0:
             doc_ref = self.db.document(path)
             return self._getDoc(doc_ref, recursive)
@@ -52,21 +62,22 @@ class Odr:
 
             return data
 
-    def set(self, path, data):
+    def set(self, path: str, data: dict) -> dict:
         doc_ref = self.db.document(path)
         doc_ref.set(data)
         return data
 
-    def update(self, path, data):
+    def update(self, path: str, data: dict) -> dict:
         doc_ref = self.db.document(path)
         doc_ref.set(data, merge=True)
         return self.get(path)
 
-    def delete(self, path):
+    def delete(self, path: str) -> None:
         if len(path.split("/")) % 2 == 0:
             ref = self.db.document(path)
         else:
             ref = self.db.collection(path)
-        print(ref)
-        self.db.recursive_delete(reference=ref, bulk_writer=self.db.bulk_writer())
-
+        self.db.recursive_delete(
+                reference=ref,
+                bulk_writer=self.db.bulk_writer()
+                )
