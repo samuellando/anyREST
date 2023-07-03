5fbb5f6fd514d9dbe622b95a99bdc551bfc6fa2d
Samuel Lando <samuel.lando@aol.com>
2023-07-03 15:08:21 -0400

    Add testing backend, mock mongoDB

---
+++ testing.py
@@ -0,0 +1,7 @@
+from mongodb import AnyrestHandlersMongoDB
+import mongomock
+
+class AnyrestHandlersTesting(AnyrestHandlersMongoDB):
+    def __init__(self):
+        super(AnyrestHandlersTesting, self).__init__(mongomock.MongoClient().db)
+
